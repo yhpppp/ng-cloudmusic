db46d1a34f57b58192337c6187b2b544edcb8900
yhpp <yhpp1995@gmail.com>
2019-12-29 15:54:11 +0800

    feat: 增加底部播放器组件

--- src/app/share/wy-ui/wy-ui.module.ts
+++ src/app/share/wy-ui/wy-ui.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SingleSheetComponent } from './single-sheet/single-sheet.component';
 import { PlayCountPipe } from '../play-count.pipe';
+import { WyPlayerComponent } from './wy-player/wy-player.component';
 
 @NgModule({
-  declarations: [SingleSheetComponent, PlayCountPipe],
+  declarations: [SingleSheetComponent, PlayCountPipe, WyPlayerComponent],
   imports: [CommonModule],
-  exports: [SingleSheetComponent, PlayCountPipe]
+  exports: [SingleSheetComponent, PlayCountPipe, WyPlayerComponent]
 })
 export class WyUiModule {}
